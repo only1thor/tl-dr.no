d5085bd55c333b6bbad29f38e4fe9d810a1cd50e
only1thor <>
2024-11-09 14:09:58 +0000

    nix shell customizations

--- shell.nix
+++ shell.nix
@@ -4,10 +4,16 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     mdbook
     mdbook-mermaid
+    fish
   ];
 
   shellHook = ''
-    echo "mdbook serve (mktemp -d)" to start a local server (bulid the book in a temporary directory)"
-    echo "add --open to open the browser"
+    echo "##### Tips #####"
+    echo "Start a local server:"
+    echo "  mdbook serve (mktemp -d) --open"
+    echo "################"
+    fish 
+    echo "nix shell exited"
+    exit 0 # exit bash when the exiting fish shell
   '';
 }
